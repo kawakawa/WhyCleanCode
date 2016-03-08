8f013133fcd295e2a7eede7b3915ed0b5aaa88d9
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-03-08 13:43:40 +0900

    パターン１のプロジェクト追加

---
+++ WhyCleanCode/App1/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class MainClass
+    {
+        public MainClass()
+        {
+        }
+    }
+}
