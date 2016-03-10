74c99d68f2af32fa63557c0ac1d5dc7e2674134e
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-03-10 16:40:09 +0900

    基本条件を満たす入場者タイプ毎の入場料を取得ロジックを追加

--- WhyCleanCode/App1/MainClass.cs
+++ WhyCleanCode/App1/MainClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,26 @@ namespace App1
 {
     public class MainClass
     {
-        public MainClass()
+
+        /// <summary>
+        /// 入場者タイプごとの入場料を取得
+        /// </summary>
+        /// <param name="personType">入場者タイプ</param>
+        /// <returns>入場料</returns>
+        public int AdmissionFee(PersonType personType)
         {
+            switch (personType)
+            {
+                case PersonType.大人:
+                    return 1000;
+                case PersonType.学生:
+                    return 700;
+                case PersonType.子供:
+                    return 500;
+                default:
+                    //該当する入場者タイプが無い場合
+                    throw new ArgumentOutOfRangeException(nameof(personType), personType, null);
+            }
         }
     }
 }
---
+++ WhyCleanCode/App1/PersonType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public enum PersonType
+    {
+        大人,
+        学生,
+        子供
+    }
+}
