c363acbe0ae2525bda0d30254ae818284320f198
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-03-10 18:13:39 +0900

    追加条件(1)を実装

---
+++ WhyCleanCode/App2/MainClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class MainClass
+    {
+        /// <summary>
+        /// 入場者タイプごとの入場料を取得
+        /// </summary>
+        /// <param name="personType">入場者タイプ</param>
+        /// <returns>入場料</returns>
+        public int AdmissionFee(PersonType personType)
+        {
+
+            //時計を用意
+            var clock=new Clock();
+            
+            //夕方以降の場合
+            if (clock.IsEvening())
+            {
+                //夕方以外の場合
+                switch (personType)
+                {
+                    case PersonType.大人:
+                        return 700;
+                    case PersonType.学生:
+                        return 400;
+                    case PersonType.子供:
+                        return 200;
+                    default:
+                        //該当する入場者タイプが無い場合
+                        throw new ArgumentOutOfRangeException(nameof(personType), personType, null);
+                }
+            }
+
+
+            //夕方以外の場合
+            switch (personType)
+            {
+                case PersonType.大人:
+                    return 1000;
+                case PersonType.学生:
+                    return 700;
+                case PersonType.子供:
+                    return 500;
+                default:
+                    //該当する入場者タイプが無い場合
+                    throw new ArgumentOutOfRangeException(nameof(personType), personType, null);
+            }
+        }
+    }
+}
---
+++ WhyCleanCode/App2/PersonType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public enum PersonType
+    {
+        大人,
+        学生,
+        子供
+    }
+}
