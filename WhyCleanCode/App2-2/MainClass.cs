74eaa63718e43d45e44c0c7fc494a3776e7fde1e
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-03-11 16:43:41 +0900

    追加条件（１）実装をリファクタリングするApp2-2を新規作成

---
+++ WhyCleanCode/App2-2/MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2_2
+{
+    /// <summary>
+    /// 追加条件（１）実装をリファクタリング
+    /// </summary>
+    public class MainClass
+    {
+    }
+}
