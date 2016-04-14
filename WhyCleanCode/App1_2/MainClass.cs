ee109bd04db2726ef40ef966d7d7940364bda9e9
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-04-14 16:33:08 +0900

    App1をリファクタリングするApp1_2プロジェクトを追加

---
+++ WhyCleanCode/App1_2/MainClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1_2
+{
+    /// <summary>
+    /// 基本情報を実装
+    /// （HowよりWhatに意図を意識した版）
+    /// </summary>
+    public class MainClass
+    {
+    }
+}
