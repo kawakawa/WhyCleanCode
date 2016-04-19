0d895b451fcca75f5b7c40aceb7b9fcca752eb40
TakuyaKawabe <rapidarrow2000@gmail.com>
2016-04-19 13:06:41 +0900

    メソッドの説明文追加

--- WhyCleanCode/App1_2/AdmissionFee/AdmissionFeeFactiory.cs
+++ WhyCleanCode/App1_2/AdmissionFee/AdmissionFeeFactiory.cs
@@ -4,16 +4,19 @@ namespace App1_2.AdmissionFee
 {
     internal class AdmissionFeeFactiory
     {
+        /// <summary>
+        /// 入場料クラス生成
+        /// </summary>
+        /// <param name="personType">入場者タイプ</param>
+        /// <returns></returns>
         public static AdmissionFee Make(PersonType personType)
         {
             //入場者タイプの条件作成
             var personTypeCondition = PersonTypeFactory.Make(personType);
-
-
+            
             //入場料のポリシー作成
             var policy = new Policy.Policy();
-
-
+            
             return new AdmissionFee(personTypeCondition,policy);
         }
     }
